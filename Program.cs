2c2c3e5ca0b3d8509364be2f14b27bc3d2b23253
Benson <bensonbird2@gmail.com>
2022-01-26 15:28:57 -0700

    ListsPractices!

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Listpractice
 {
@@ -6,7 +7,21 @@ namespace Listpractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // list 
+            var listOFNeeds = new List<string>();
+            //adding to list
+            listOFNeeds.Add("prototype");
+            listOFNeeds.Add("book");
+            listOFNeeds.Add("to be deleted");
+            //preint only one of them
+            //Console.WriteLine(listOFNeeds[0]);
+            // to remove from the list
+            listOFNeeds.RemoveAt(2);
+            
+            foreach (string item in listOFNeeds)
+            {
+            Console.WriteLine(item);
+            }
         }
     }
 }
