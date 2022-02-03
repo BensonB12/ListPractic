f7e3ef2bfaf294b9d255265f5bac19b237b7111f
Benson <bensonbird2@gmail.com>
2022-02-02 17:23:39 -0700

    writing a book

--- WorkingWithTextFile/Program.cs
+++ WorkingWithTextFile/Program.cs
@@ -1,4 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("to exit press \"x\"");
+            var allLines =File.ReadAllLines("Ston.txt");
+            var textReader = new StreamReader("Ston.txt");
+            string firstLine = textReader.ReadLine();
+            
+            foreach (var item in allLines)
+            {
+            Console.WriteLine(item);
+            }
+            
+            textReader.Close();
+            
+            if (firstLine == " ")
+            {
+                var textWriter = new StreamWriter("Ston.txt");
+                textWriter.WriteLine("Ston");
+                textWriter.Close();
+            }
+
+            while (true)
+            {
+                var list = new List<string>();
+                string input = Console.ReadLine();
+               
+                if (input == "x")
+                {
+                    return;
+                }
+
+                list.Add(input);
+                File.AppendAllLines("Ston.txt", list);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    var list = new List<string>();
+//    list.Add(".");
+//    list.Add("I love writing books!");
+
+//    File.AppendAllLines("Ston.txt", list); 
